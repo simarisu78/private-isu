93f446a6d75d8eb971deba8c13982c1107004e75
admin <test@example.com>
2024-06-18 02:17:54 +0900

    fix: getPostsの全行取得を制限

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -524,7 +524,7 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC", t.Format(ISO8601Format))
+	err = db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` AS p JOIN users AS u ON p.user_id = u.id WHERE p.created_at <= ? AND u.del_flg = 0 ORDER BY p.created_at DESC LIMIT 20", t.Format(ISO8601Format))
 	if err != nil {
 		log.Print(err)
 		return
