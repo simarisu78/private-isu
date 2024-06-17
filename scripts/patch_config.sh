ee4ea60e975f3b4e91f0628ab4655a132e640ac5
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2024-06-17 21:31:38 +0900

    fix: configのパスを修正

--- scripts/patch_config.sh
+++ scripts/patch_config.sh
@@ -2,5 +2,5 @@
 
 set -e
 cd `dirname $0`
-sudo cp ../mysql/mysql.conf.d/slowlog.cnf /etc/mysql/mysql.conf.d/slowlog.cnf
-sudo cp ../nginx/conf.d/defaults.conf /etc/nginx/conf.d/defaults.conf
+sudo cp ../etc/mysql/mysql.conf.d/slowlog.cnf /etc/mysql/mysql.conf.d/slowlog.cnf
+sudo cp ../etc/nginx/conf.d/defaults.conf /etc/nginx/conf.d/defaults.conf
